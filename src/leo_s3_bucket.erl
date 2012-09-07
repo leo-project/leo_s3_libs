bd133f9d4eb3bf9625dd2f6d4f13a88d1aee6514
yoshiyuki.kanno <yoshiyuki.kanno@stoic.co.jp>
2012-09-07 01:35:10 +0000

    fix a bug when doing rpc:nb_yield

--- src/leo_s3_bucket.erl
+++ src/leo_s3_bucket.erl
@@ -346,7 +346,7 @@ find_buckets_by_id_2(AccessKey, DB, Node, Value0, CRC) ->
                   not_found;
               {value, {error, Cause}} ->
                   {error, Cause};
-              {badrpc, Cause} ->
+              {value, {badrpc, Cause}} ->
                   {error, Cause};
               {'EXIT', Cause} ->
                   {error, Cause}

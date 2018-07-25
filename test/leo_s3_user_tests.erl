4f2a7b2afe323ac1530e6d7dee7c05da8386efe1
Yosuke Hara <yousuke.hara@gmail.com>
2018-07-25 16:13:56 +0900

    Fixed for issue#965

--- test/leo_s3_user_tests.erl
+++ test/leo_s3_user_tests.erl
@@ -2,7 +2,7 @@
 %%
 %% Leo S3-Libs
 %%
-%% Copyright (c) 2012-2015 Rakuten, Inc.
+%% Copyright (c) 2012-2018 Rakuten, Inc.
 %%
 %% This file is provided to you under the Apache License,
 %% Version 2.0 (the "License"); you may not use this file
@@ -65,35 +65,53 @@ suite_() ->
     ok = leo_s3_user_credential:create_table(ram_copies, [node()]),
     catch leo_s3_auth:create_table(ram_copies, [node()]),
 
-    UserId    = <<"Name is Leo">>,
-    Password0 = <<"Type is FS">>,
+    %% create a user
+    UserId_1 = <<"Leo">>,
+    Password_1 = <<"FastStorage">>,
+    {ok, Keys_1} = leo_s3_user:create(UserId_1, Password_1),
+    AccessKeyId_1 = leo_misc:get_value('access_key_id', Keys_1),
+    SecretAccessKey_1 = leo_misc:get_value('secret_access_key', Keys_1),
 
-    %% %% create-user
-    {ok, Keys} = leo_s3_user:put(UserId, Password0, true),
-    AccessKeyId     = leo_misc:get_value('access_key_id',     Keys),
-    SecretAccessKey = leo_misc:get_value('secret_access_key', Keys),
+    ?assertEqual(2, length(Keys_1)),
+    ?assertEqual(true, AccessKeyId_1 /= <<>>),
+    ?assertEqual(true, SecretAccessKey_1 /= <<>>),
 
-    ?assertEqual(2, length(Keys)),
-    ?assertEqual(true, AccessKeyId     /= <<>>),
+    {ok, #?S3_USER{id = UserId_1,
+                   password = PWHash_1,
+                   role_id = ?ROLE_GENERAL,
+                   del = false}} = leo_s3_user:find_by_id(UserId_1),
+    ?assertEqual(true, erlpass:match(Password_1, PWHash_1)),
+
+
+    %% create-user
+    UserId_2 = <<"Name is Leo">>,
+    Password_2 = <<"Type is FS">>,
+
+    {ok, Keys_2} = leo_s3_user:create(UserId_2, Password_2),
+    AccessKeyId = leo_misc:get_value('access_key_id', Keys_2),
+    SecretAccessKey = leo_misc:get_value('secret_access_key', Keys_2),
+
+    ?assertEqual(2, length(Keys_2)),
+    ?assertEqual(true, AccessKeyId /= <<>>),
     ?assertEqual(true, SecretAccessKey /= <<>>),
 
     {error,already_exists} =
-        leo_s3_user:put(UserId, Password0, true),
+        leo_s3_user:create(UserId_2, Password_2),
 
-    %% %% migration from 0.12.7 to 1.3.3
-    {ok, _} = leo_s3_user:put(<< UserId/binary, "_md5" >>, Password0, true, md5),
-    {ok, ResMD5} = leo_s3_user:find_by_id(<< UserId/binary, "_md5" >>),
+    %% migration from 0.12.7 to 1.3.3
+    {ok, _} = leo_s3_user:create(<< UserId_2/binary, "_md5" >>, Password_2, 'md5'),
+    {ok, ResMD5} = leo_s3_user:find_by_id(<< UserId_2/binary, "_md5" >>),
     ?debugVal(ResMD5),
-    {ok, _} = leo_s3_user:auth(<< UserId/binary, "_md5" >>, Password0),
-    {ok, ResBcrypted} = leo_s3_user:find_by_id(<< UserId/binary, "_md5" >>),
+    {ok, _} = leo_s3_user:auth(<< UserId_2/binary, "_md5" >>, Password_2),
+    {ok, ResBcrypted} = leo_s3_user:find_by_id(<< UserId_2/binary, "_md5" >>),
     ?debugVal(ResBcrypted),
 
-    %% %% migration from 1.3.2 to 1.3.3
-    {ok, _} = leo_s3_user:put(<< UserId/binary, "_md5_w/salt" >>, Password0, true, md5_with_salt),
-    {ok, ResMD5WithSalt} = leo_s3_user:find_by_id(<< UserId/binary, "_md5_w/salt" >>),
+    %% migration from 1.3.2 to 1.3.3
+    {ok, _} = leo_s3_user:create(<< UserId_2/binary, "_md5_w/salt" >>, Password_2, 'md5_with_salt'),
+    {ok, ResMD5WithSalt} = leo_s3_user:find_by_id(<< UserId_2/binary, "_md5_w/salt" >>),
     ?debugVal(ResMD5WithSalt),
-    {ok, _} = leo_s3_user:auth(<< UserId/binary, "_md5_w/salt" >>, Password0),
-    {ok, ResBcrypted2} = leo_s3_user:find_by_id(<< UserId/binary, "_md5_w/salt" >>),
+    {ok, _} = leo_s3_user:auth(<< UserId_2/binary, "_md5_w/salt" >>, Password_2),
+    {ok, ResBcrypted2} = leo_s3_user:find_by_id(<< UserId_2/binary, "_md5_w/salt" >>),
     ?debugVal(ResBcrypted2),
 
     %% %% import-user
@@ -101,54 +119,55 @@ suite_() ->
     ImportAccessKey = <<"importA">>,
     ImportSecretKey = <<"importS">>,
     {ok, IKeys} = leo_s3_user:import(ImportUserId, ImportAccessKey, ImportSecretKey),
-    IAccessKeyId     = leo_misc:get_value('access_key_id',     IKeys),
+    IAccessKeyId = leo_misc:get_value('access_key_id', IKeys),
     ISecretAccessKey = leo_misc:get_value('secret_access_key', IKeys),
-    ?assertEqual(2, length(Keys)),
+    ?assertEqual(2, length(IKeys)),
     ?assertEqual(IAccessKeyId, ImportAccessKey),
     ?assertEqual(ISecretAccessKey, ImportSecretKey),
 
     %% %% find-by-id
-    {ok, Res1} = leo_s3_user:find_by_id(UserId),
-    ?assertEqual(UserId, Res1#?S3_USER.id),
+    {ok, Res1} = leo_s3_user:find_by_id(UserId_2),
+    ?assertEqual(UserId_2, Res1#?S3_USER.id),
     ?debugVal(Res1#?S3_USER.password),
 
     %% %% find_by_access_key_id
     {ok, Res2} = leo_s3_user_credential:find_by_access_key_id(AccessKeyId),
-    ?assertEqual(UserId,      Res2#user_credential.user_id),
+    ?assertEqual(UserId_2, Res2#user_credential.user_id),
     ?assertEqual(AccessKeyId, Res2#user_credential.access_key_id),
 
     %% %% find_all
-    {ok, _} = leo_s3_user:put(<< UserId/binary, "_1" >>, Password0, true),
-    {ok, _} = leo_s3_user:put(<< UserId/binary, "_2" >>, Password0, true),
-    {ok, _} = leo_s3_user:put(<< UserId/binary, "_3" >>, Password0, true),
-    {ok, _} = leo_s3_user:put(<< UserId/binary, "_4" >>, Password0, true),
+    %% @OTODO
+    {ok, _} = leo_s3_user:create(<< UserId_2/binary, "_1" >>, Password_2),
+    {ok, _} = leo_s3_user:create(<< UserId_2/binary, "_2" >>, Password_2),
+    {ok, _} = leo_s3_user:create(<< UserId_2/binary, "_3" >>, Password_2),
+    {ok, _} = leo_s3_user:create(<< UserId_2/binary, "_4" >>, Password_2),
     {ok, Users_1} = leo_s3_user_credential:find_all(),
     {ok, Users_2} = leo_s3_user_credential:find_all_with_role(),
     ?debugVal(Users_1),
     ?debugVal(Users_2),
-    ?assertEqual(8, length(Users_1)),
-    ?assertEqual(8, length(Users_2)),
+    ?assertEqual(9, length(Users_1)),
+    ?assertEqual(9, length(Users_2)),
 
-    %% %% get_credential_by_id
-    {ok, Credential} = leo_s3_user_credential:get_credential_by_user_id(UserId),
-    ?assertEqual(AccessKeyId,     leo_misc:get_value(access_key_id, Credential)),
+    %% get_credential_by_id
+    {ok, Credential} = leo_s3_user_credential:get_credential_by_user_id(UserId_2),
+    ?assertEqual(AccessKeyId, leo_misc:get_value(access_key_id, Credential)),
     ?assertEqual(SecretAccessKey, leo_misc:get_value(secret_access_key, Credential)),
 
     %% auth,
-    {ok, _} = leo_s3_user:auth(UserId, Password0),
-    {error,invalid_values} = leo_s3_user:auth(UserId, <<>>),
+    {ok, _} = leo_s3_user:auth(UserId_2, Password_2),
+    {error,invalid_values} = leo_s3_user:auth(UserId_2, <<>>),
 
     %% update
-    ok = leo_s3_user:update(#?S3_USER{id = UserId,
+    ok = leo_s3_user:update(#?S3_USER{id = UserId_2,
                                       role_id = 9}),
-    {ok, Res3} = leo_s3_user:find_by_id(UserId),
-    ?assertEqual(UserId, Res3#?S3_USER.id),
-    ?assertEqual(9,      Res3#?S3_USER.role_id),
+    {ok, Res3} = leo_s3_user:find_by_id(UserId_2),
+    ?assertEqual(UserId_2, Res3#?S3_USER.id),
+    ?assertEqual(9, Res3#?S3_USER.role_id),
 
     %% delete
     {ok, ALL_1} = leo_s3_user_credential:find_all_with_role(),
-    ok = leo_s3_user:delete(UserId),
-    not_found = leo_s3_user:find_by_id(UserId),
+    ok = leo_s3_user:delete(UserId_2),
+    not_found = leo_s3_user:find_by_id(UserId_2),
     {ok, ALL_2} = leo_s3_user_credential:find_all_with_role(),
     ?assertEqual(1, length(ALL_1) - length(ALL_2)),
 
@@ -178,7 +197,7 @@ suite_() ->
     ?assertEqual(5, length(RetL_4) - length(RetL_3)),
 
     %% delete_all_related_records
-    UID4Delete = <<UserId/binary, "_1">>,
+    UID4Delete = <<UserId_2/binary, "_1">>,
 
     %% retreive access_key_id
     {ok, Creds4Delete} = leo_s3_user_credential:get_credential_by_user_id(UID4Delete),
@@ -194,12 +213,11 @@ suite_() ->
 
     ImportSecretKey2 = <<"importS2">>,
     {ok, IKeys2} = leo_s3_user:force_import(ImportUserId2, ImportAccessKey, ImportSecretKey2),
-    IAccessKeyId2     = leo_misc:get_value('access_key_id',     IKeys2),
+    IAccessKeyId2 = leo_misc:get_value('access_key_id', IKeys2),
     ISecretAccessKey2 = leo_misc:get_value('secret_access_key', IKeys2),
-    ?assertEqual(2, length(Keys)),
+    ?assertEqual(2, length(IKeys)),
     ?assertEqual(IAccessKeyId2, ImportAccessKey),
     ?assertEqual(ISecretAccessKey2, ImportSecretKey2),
-
     ok.
 
 -endif.
